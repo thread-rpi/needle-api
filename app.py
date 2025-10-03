802fc55e813eb37a7ead88cd5088bcb983416c29
milesthomas <ths22115@towerhill.org>
2025-10-03 03:53:25 -0400

    health endpoint added

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "Hello, Flask!"
+def root():
+    return "Welcome to Needle!"
+
+@app.route("/health")
+def health_check():
+    status = {"status": "healthy"}
+    http_status_code = 200
+
+    #db connection checks when added
+
+    return jsonify(status), http_status_code
 
 if __name__ == "__main__":
     app.run(debug=True)
