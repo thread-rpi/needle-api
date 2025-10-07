ee60b972e59999d3341c3861c54c50f80b409651
Vincent Tran <vincenttran565@gmail.com>
2025-10-07 16:33:53 -0400

    Added more parameter checking

--- get_members.py
+++ get_members.py
@@ -1,4 +1,5 @@
 from flask import jsonify
+from datetime import datetime, timezone
 
 def format_member(member):
     member_data = {
@@ -12,18 +13,25 @@ def format_member(member):
 
 def get_members(members, year_string):
     if len(year_string) != 2:
-        return jsonify({"error": f"Invalid parameter {year_string}"}), 400
+        return jsonify({"error": f"Invalid parameter {year_string}, string not of length 2"}), 400
     
     try:
         year = int(year_string)
     except Exception as e:
-        return jsonify({"error": f"Invalid parameter {year_string}"}), 400
+        return jsonify({"error": f"Invalid parameter {year_string}, string is not an integer"}), 400
     
+    current_year = datetime.now(timezone.utc).year - 2000
+    current_month = datetime.now(timezone.utc).month
+    if current_month <= 5:
+        current_year -= 1
+    if not(year >= 25 and year <= current_year):
+        return jsonify({"error": f"Invalid parameter {year_string}, year out of valid range"}), 400
+
     try:
         member_list = list(members.find({"year": year}))
         if len(member_list):
             return jsonify({"data": [format_member(member) for member in member_list]})
         else:
-            return jsonify({"message": f"No members for year {year} found"}), 404
+            return jsonify({"message": f"No members for year {year} found"}), 500
     except Exception as e:
         return jsonify({"error": str(e)}), 500
