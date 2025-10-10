b0f26a2c176aff433b9be4ec88f3f19f19f76a7f
Vincent Tran <vincenttran565@gmail.com>
2025-10-10 13:13:37 -0400

    Added comments

--- get_members.py
+++ get_members.py
@@ -1,6 +1,8 @@
 from flask import jsonify
 from datetime import datetime, timezone
 
+# member: a member document
+# Parses memeber data into a dictionary
 def format_member(member):
     member_data = {
         "_id": str(member["_id"]),
@@ -11,15 +13,21 @@ def format_member(member):
     }
     return member_data
 
+# members: MongoDb member collection
+# year_string: String representing a school year of form [yy]
+# returns all memebr data for the given year
 def get_members(members, year_string):
+    # Check if string is correct length
     if len(year_string) != 2:
         return jsonify({"error": f"Invalid parameter {year_string}, string not of length 2"}), 400
     
+    # Check if string can be converted to an int
     try:
         year = int(year_string)
     except Exception as e:
         return jsonify({"error": f"Invalid parameter {year_string}, string is not an integer"}), 400
     
+    # Check if the given year is inbetween 25 (2025) and the current school year
     current_year = datetime.now(timezone.utc).year - 2000
     current_month = datetime.now(timezone.utc).month
     if current_month <= 5:
@@ -27,6 +35,7 @@ def get_members(members, year_string):
     if not(year >= 25 and year <= current_year):
         return jsonify({"error": f"Invalid parameter {year_string}, year out of valid range"}), 400
 
+    # Return the data of all members of a given year
     try:
         member_list = list(members.find({"year": year}))
         if len(member_list):
