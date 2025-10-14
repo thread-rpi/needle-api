79ede97426107730d31907243ca0bb308864a1ca
milesthomas <ths22115@towerhill.org>
2025-10-13 23:07:06 -0400

    added ACAO header, tweaked health endpoint to standardize responses

--- app.py
+++ app.py
@@ -6,26 +6,31 @@ from get_shoot import get_shoot
 from get_members import get_members
 from current_fotw import current_fotw
 
-SERVER_TIMEOUT = 5000 # client will error if a connection isn't made within 5 seconds of its first request
+# client will error if a connection isn't made within 5 seconds of its first request
+SERVER_TIMEOUT = 5000 
 
 # Initialize flask application
 app = Flask(__name__)
 
-# MongoDB Configuration
+# MongoDB configuration
 app.config['MONGO_URI'] = os.getenv('MONGO_URI')
-client = pymongo.MongoClient(app.config['MONGO_URI'], serverSelectionTimeoutMS=SERVER_TIMEOUT)
 
-# Initialize databases
+# Initialize mongodb databases and collections
+client = pymongo.MongoClient(app.config['MONGO_URI'], serverSelectionTimeoutMS=SERVER_TIMEOUT)
 eventsDB = client['eventsDB']
+fotDB = client['fotDB']
+memberDB = client['memberDB']
 shoots = eventsDB['shoot']
 events = eventsDB['event']
-
-fotDB = client['fotDB']
 fot = fotDB['fot']
-
-memberDB = client['memberDB']
 member = memberDB['member']
-
+ 
+@app.after_request
+# Globally modify response headers before requests
+def add_header(response):
+    # Allowing resource access to URIs specified in .env
+    response.headers['Access-Control-Allow-Origin'] = os.getenv('ACAO_URI') if os.getenv('ACAO_URI') else '*'
+    return response
 
 # Routes
 @app.route("/")
@@ -35,20 +40,20 @@ def root():
 
 @app.route("/health")
 def health_check():
-    status = {"status": "healthy"}
+    res = {"data": "healthy"}
     http_status_code = 200
 
     # DB connection checks
     try:
        client.admin.command('ping')
     except ConnectionFailure:
-       status = {"status": "unhealthy", "error": "MongoDB connection failed."}
+       res = {"error": "MongoDB connection failed."}
        http_status_code = 500
     except OperationFailure:
-        status = {"status": "unhealthy", "error": "MongoDB authentication failed."}
+        res = {"error": "MongoDB authentication failed."}
         http_status_code = 500
 
-    return jsonify(status), http_status_code
+    return jsonify(res), http_status_code
 
 
 @app.route("/api/shoot/<shoot_id>", methods=["GET"])
